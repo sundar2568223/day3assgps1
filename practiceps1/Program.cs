cbced227d523b91c5ac4e8f24ebc2e5235ab8c86
sunder2568223 <sunderyssr@gmail.com>
2023-07-12 15:21:16 +0530

    first commit

---
+++ practiceps1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace practiceps1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string strNumber = "123";
+            int result;
+
+            if (int.TryParse(strNumber, out result))
+            {
+                Console.WriteLine("Successfully converted to integer: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Failed to convert to integer.");
+            }*/
+            /* string strNumber = "3.14";
+             double result;
+
+             try
+             {
+                 result = Convert.ToDouble(strNumber);
+                 Console.WriteLine("Successfully converted to double: " + result);
+             }
+             catch (FormatException)
+             {
+                 Console.WriteLine("Invalid format. Failed to convert to double.");
+             }
+             catch (OverflowException)
+             {
+                 Console.WriteLine("Value is too large or too small. Failed to convert to double.");
+             }*/
+            /*string strNumber = "3.14";
+            decimal result;
+
+            try
+            {
+                result = decimal.Parse(strNumber);
+                Console.WriteLine("Successfully converted to decimal: " + result);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format. Failed to convert to decimal.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Value is too large or too small. Failed to convert to decimal.");
+            }*/
+            string strNumber = "123";
+            int newstring;
+            int result;
+
+            if (int.TryParse(strNumber, out result))
+            {
+                newstring = result*3;
+                Console.WriteLine("Successfully converted to integer: " + result);
+
+            }
+            else
+            {
+                if(strNumber== "123")
+                {
+                    newstring = strNumber * 3;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Failed to convert to integer.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
